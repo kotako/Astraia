29725f802f5e1588c6812a8def038613d0519445
Takorras <sy.empress0321@gmail.com>
2018-05-05 17:45:08 +0900

    Create SearchView

--- app/src/main/java/io/gitub/kotako/astraia/articles/ArticlesActivity.kt
+++ app/src/main/java/io/gitub/kotako/astraia/articles/ArticlesActivity.kt
@@ -1,10 +1,16 @@
 package io.gitub.kotako.astraia.articles
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.support.v7.widget.SearchView
+import android.view.Menu
+import android.view.MenuItem
 import dagger.android.support.DaggerAppCompatActivity
 import io.gitub.kotako.astraia.R
 import io.gitub.kotako.astraia.data.source.ArticleRepository
 import io.gitub.kotako.astraia.di.ViewModelFactory
+import kotlinx.android.synthetic.main.activity_articles.*
 import javax.inject.Inject
 
 class ArticlesActivity: DaggerAppCompatActivity(), ArticlesNavigator {
@@ -19,6 +25,8 @@ class ArticlesActivity: DaggerAppCompatActivity(), ArticlesNavigator {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_articles)
 
+        setSupportActionBar(toolbar)
+
         val fragment = ArticlesFragment.newInstance()
         viewModel.setNavigator(this)
         fragment.setViewModel(viewModel)
@@ -26,5 +34,16 @@ class ArticlesActivity: DaggerAppCompatActivity(), ArticlesNavigator {
             add(R.id.container, fragment)
             commit()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.search, menu)
+        val view = (menu.findItem(R.id.search) as MenuItem).actionView as SearchView
+        val searchService = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        view.setSearchableInfo(searchService.getSearchableInfo(componentName))
+        view.isIconified = true
+
+        return true
     }
 }
