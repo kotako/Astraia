f38b1e56f862b101d89347266f65319498a095f2
Takorras <sy.empress0321@gmail.com>
2018-05-08 16:25:25 +0900

    Create MainActivity

--- app/src/main/java/io/gitub/kotako/astraia/MainActivity.kt
+++ app/src/main/java/io/gitub/kotako/astraia/MainActivity.kt
@@ -1,12 +1,25 @@
 package io.gitub.kotako.astraia
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        fun start(context: Context) {
+            context.startActivity(Intent(context, MainActivity::class.java))
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar.setNavigationIcon(R.drawable.ic_menu)
+        setSupportActionBar(toolbar)
     }
+
+
 }
